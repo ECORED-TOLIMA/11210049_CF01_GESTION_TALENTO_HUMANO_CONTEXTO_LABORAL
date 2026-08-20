f922e5c230fcf751d4df51b4500c3f86d09d2425
mafechoSena <mafechoSena@users.noreply.github.com>
2026-08-20 11:49:21 -0500

    sintesis

--- src/views/Sintesis.vue
+++ src/views/Sintesis.vue
@@ -1,29 +1,28 @@
 <template lang="pug">
 .curso-main-container.pb-3
   BannerInterno(icono="fas fa-sitemap" titulo="Síntesis")
-  .container.tarjeta.tarjeta--blanca.p-4.p-md-5
-    p Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eu tincidunt erat. Fusce cursus mauris in nulla egestas vulputate. Phasellus viverra accumsan libero, sed luctus diam maximus euismod. Mauris sem risus, tincidunt vel libero in, lobortis commodo ante. Sed pulvinar luctus orci in rhoncus. Nulla ut dapibus risus, ac tempor est. Donec nulla augue, vehicula quis malesuada vitae, fermentum non nisi. Donec tincidunt nibh a magna bibendum rutrum. 
-    
+  .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5
 
-    .row.justify-content-center
-      .col-lg-10.mb-5.bgs.p-4.brad
-        figure
-          img(src="@/assets/curso/sintesis.svg", alt="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eu tincidunt erat. Fusce cursus mauris in nulla egestas vulputate. Phasellus viverra accumsan libero, sed luctus diam maximus euismod. Mauris sem risus, tincidunt vel libero in, lobortis commodo ante. Sed pulvinar luctus orci in rhoncus. Nulla ut dapibus risus, ac tempor est. Donec nulla augue, vehicula quis malesuada vitae, fermentum non nisi. Donec tincidunt nibh a magna bibendum rutrum. ")
+    p(data-aos="fade-down") La gestión del talento humano alcanza su sentido pleno cuando se fundamenta en criterios éticos y en una comprensión respetuosa de la persona. El componente articula cuatro ejes temáticos: los fundamentos de la ética como base del comportamiento humano y organizacional; las relaciones interpersonales en el trabajo, sustentadas en la alteridad y el respeto; la gestión del talento humano en su vínculo con la cultura, las políticas internas y el clima organizacional; y la inteligencia emocional como capacidad para reconocer y regular las emociones en el entorno laboral. Estos ejes se articulan para evidenciar que gestionar personas desde la ética no es una opción secundaria, sino una condición necesaria para construir organizaciones más humanas, coherentes y sostenibles.
+
+    p.mt-4.mb-0(data-aos="fade-down") A continuación, se presenta el mapa conceptual que organiza de manera visual los ejes temáticos del componente y las relaciones que los articulan:
+
+    //- El mapa del XD mide 1245 px y el ancho útil de la tarjeta es 1228: se ajusta a él.
+    figure.mt-4(data-aos="fade-down")
+      img(src="@/assets/curso/sintesis.svg" alt="Mapa conceptual sobre la ética y la gestión del talento humano. Integra cuatro ejes: la ética, las relaciones interpersonales, la gestión del talento humano y la inteligencia emocional, que juntos fortalecen la convivencia, el clima laboral y la sostenibilidad de la organización." style="width: 1228px").m-auto
+
+    .row.justify-content-center.mt-5
       .col-auto
-        a.anexo.mb-5(:href="obtenerLink('/downloads/Sintesis.pdf')" target="_blank")
+        a.anexo(:href="obtenerLink('/downloads/Sintesis.pdf')" target="_blank")
           .anexo__icono
             img(src="@/assets/bullets/icono-pdf.svg")
           .anexo__texto
             p Anexo. Síntesis
-
 </template>
 
 <script>
 export default {
   name: 'Sintesis',
-  data: () => ({
-    // variables de vue
-  }),
   mounted() {
     this.$nextTick(() => {
       this.$aosRefresh()
